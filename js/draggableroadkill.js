8b74a7f277c758d62bbb90c9b8c3e704b08a9e07
dreamhomehomes <anyaerosen@gmail.com>
2017-11-03 23:18:51 -0400

    initial commit

--- js/draggableroadkill.js
+++ js/draggableroadkill.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $( '#mistakes, #headless, #vulture' ).draggable({
-    cursor: "move",
-    start: function(event, ui) { $(this).css("z-index", a++); }
+    cursor: "move"
   });
 } );
