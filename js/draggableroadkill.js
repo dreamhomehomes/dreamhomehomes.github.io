c013a94b9f93dd0a02e4b83dbb8da54c96ff7c68
dreamhomehomes <33168579+dreamhomehomes@users.noreply.github.com>
2017-11-06 22:58:45 -0500

    Update draggableroadkill.js

--- js/draggableroadkill.js
+++ js/draggableroadkill.js
@@ -1,7 +1,6 @@
 (document).ready(function() {
   $( '#mistakes, #headless, #vulture' ).draggable({
     cursor: "move",
-    stack: '#mistakes, #headless, #vulture',
-    containment: "#containmentwrapper",
+    stack: '#mistakes, #headless, #vulture'
   });
 } );
